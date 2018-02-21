a39e6ef4b0d746a32afacf0131ca4ee8105cc5fd
Chris Harvey <chharvey@icloud.com>
2018-02-21 08:03:06 +0000

    automatically backed up by learn

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -5,10 +5,12 @@ class OwnersController < ApplicationController
     erb :'/owners/index'
   end
 
+
   get '/owners/new' do
     @pets = Pet.all
     erb :'/owners/new'
   end
+
 
   post '/owners' do
     @owner = Owner.create(params["owner"])
@@ -19,15 +21,18 @@ class OwnersController < ApplicationController
     redirect to "/owners/#{@owner.id}"
   end
 
+
   get '/owners/:id/edit' do
     @owner = Owner.find(params[:id])
     erb :'/owners/edit'
   end
 
+
   get '/owners/:id' do
     @owner = Owner.find(params[:id])
     erb :'/owners/show'
   end
+
 
   post '/owners/:id' do
     @owner = Owner.find(params[:id])
